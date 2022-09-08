b93a0236b3370e2e2a6818ba748c4e869037ecf5
Rafael Elias <rafaelniza2000@hotmail.com>
2022-09-07 21:38:42 -0300

    remove files

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Header = ({ course = "" }) => <h1>{course}</h1>;
+
+const Part = ({ name, exercises }) => <p> {name}: {exercises} </p>;
+
+const Content = ({ parts }) => parts.map(part => <Part key={part.name} name={part.name} exercises={part.exercises} />);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((cur, acc) => cur += acc.exercises, 0);
+  return <p>Number of exercises: {total} </p>;
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ]
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
-}
+};
 
 export default App;
